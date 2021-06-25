6acdb35b0e68e6287376d86ed3ecd08f3c2beb99
Mill <millerkjc@gmail.com>
2021-06-25 14:12:51 +0200

    [LD FLASH] flash section + flash code removed

--- zcfs_stm32/Middlewares/Third_Party/ZCFS/src/fs_core.c
+++ zcfs_stm32/Middlewares/Third_Party/ZCFS/src/fs_core.c
@@ -85,66 +85,7 @@ void fs_init(UART_HandleTypeDef *huart, DMA_HandleTypeDef *hdma_usart_tx, DMA_Ha
 //	flash_write(superblock_pointer_t, (uint32_t)superblock_t[0], sizeof(ifile_t));
 
 //
-	char* s = "ciao mammaaaaa";
-	char* s2 = "hello mammaaaaa";
 
-
-//	 uint32_t SectorError;
-//	 FLASH_EraseInitTypeDef EraseInitStruct;
-//	 EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
-//	 EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
-//	 EraseInitStruct.Sector = FLASH_SECTOR_6; //Specify sector number
-//	 EraseInitStruct.NbSectors = 1; //This is also important!
-//	 if(HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError) != HAL_OK) {
-//		 //Erase error!
-//			 sprintf(s, "bbbb");
-//	 }
-//
-	 FLASH_Erase_Sector(FLASH_SECTOR_6,VOLTAGE_RANGE_4);
-	 FLASH_Erase_Sector(FLASH_SECTOR_7,VOLTAGE_RANGE_4);
-	 FLASH_Erase_Sector(FLASH_SECTOR_8,VOLTAGE_RANGE_4);
-	 FLASH_Erase_Sector(FLASH_SECTOR_9,VOLTAGE_RANGE_1);
-	 FLASH_Erase_Sector(FLASH_SECTOR_10,VOLTAGE_RANGE_1);
-	 FLASH_Erase_Sector(FLASH_SECTOR_11,VOLTAGE_RANGE_1);
-//
-//
-//	 EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
-//	 EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
-//	 EraseInitStruct.Sector = FLASH_SECTOR_7; //Specify sector number
-//	 EraseInitStruct.NbSectors = 1; //This is also important!
-//	 if(HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError) != HAL_OK) {
-//		 //Erase error!
-//			 sprintf(s, "bbbb");
-//	 }
-
-
-
-
-	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP    | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR| FLASH_FLAG_PGSERR);
-
-	flash_write(superblock_pointer_t, (uint32_t)s, strlen(s));
-	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP    | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR| FLASH_FLAG_PGSERR);
-
-	flash_write(0x0805fffc, (uint32_t)s, strlen(s));
-//	flash_write(0x0805fffc, (uint32_t)s2, strlen(s2));
-//	flash_write(0x0805fffc, (uint32_t)s, strlen(s));
-
-	flash_write(0x08060000, (uint32_t)s, strlen(s));
-	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP    | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR| FLASH_FLAG_PGSERR);
-
-	flash_write(0x08041000, (uint32_t)s, strlen(s));
-
-	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP    | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR| FLASH_FLAG_PGSERR);
-
-//	char msg[30] = "hello";
-
-
-//	TODO Uncomment when released?
-//	uint32_t address = (uint32_t)_VZCFS_DISK_START;
-//	for(int DataIdx = 0; DataIdx < (uint32_t)_VZCFS_DISK_SIZE; DataIdx++)
-//	{
-//	    HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, address++, 0xF);
-//	}
 
 }
 
