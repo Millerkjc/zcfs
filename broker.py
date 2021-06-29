bdfaaad3d13f59063228b0b6f17c63a69c4ab316
Mill <millerkjc@gmail.com>
2021-06-30 01:00:59 +0200

    [broker] prints beautified

--- broker.py
+++ broker.py
@@ -16,47 +16,78 @@ disk_size = 1024  # 5 MB
 #disk_size = 1024 * 1024 * 5 # 5 MB
 disk = [b'\x00'] * disk_size
 
+def print_sys_configuration():
+    conf_title = ['# system variables', '# disk variables']
+    conf_info = {
+                 0:[( 'base dir', base_dir)],
+                 1:[('disk size', disk_size)]
+                }
+
+    conf_max_len = [len(max([s for (s,d) in conf_info[els]], key=len)) for els in range(len(conf_title))]
+
+    for idx in range(len(conf_title)):
+        print('{}'.format(conf_title[idx]))
+        for el in conf_info[idx]:
+            print(('{}'.format(el[0])).rjust(conf_max_len[idx]) + ': {}'.format(el[1]))
+    print()
+
+
+def print_tty_configuration(args):
+    print('port set to:{}{}\nbaudrate set to: {}'.format(' '*5, args_in.tty_port, args_in.tty_speed))
+
+def print_pkt_metadata(pkt):
+    # pkt header
+    header = pkt[:header_size]
+    # disk address
+    raw_addr = pkt[header_size : header_size+address_buf]
+    disk_addr = '0x' +''.join(['{:02x}'.format(x) for x in raw_addr[::-1]])
+    # pkt data
+    pkt_data = pkt[header_size+address_buf : - eol_size]
+
+    infos = [
+             (  'raw packet',       pkt),
+             (  'pkt header',    header),
+             ('disk address', disk_addr),
+             (    'pkt body',  pkt_data)
+            ]
+    inf_max_len = len(max([s for (s,d) in infos], key=len))
+
+    for s,d in infos:
+        print(('{}'.format(s)).rjust(inf_max_len) + ': {}'.format(d))
+    print()
+
+
 def write_to_disk(address, data):
     for (pos,d) in enumerate(data):
         disk[address + pos] = bytes([d])
 
-# TODO
 def print_disk(p_disk_size=4):
     for address in range(0, disk_size, p_disk_size):
-        #print(('{} ' + ' {}'*4).format(hex(x), disk[x], disk[x+1], disk[x+2], disk[x+3]))
-        #print(('0x{:03x}: ' + ' {:02x}'*p_disk_size).format(x, ord(disk[x]), ord(disk[x+1]), ord(disk[x+2]), ord(disk[x+3])))
         print(('0x{:03x}: ' + ' {:02x}'*p_disk_size).format(address, *[ord(disk[idx]) for idx in range(address, address+p_disk_size)]))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('tty_port', help='tty of the connected STM32 board')
-parser.add_argument('tty_speed', type=int, help='baudrate tty of the connected STM32 board')
-parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tty_port', help='tty of the connected STM32 board')
+    parser.add_argument('tty_speed', type=int, help='baudrate tty of the connected STM32 board')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    args = parser.parse_args()
+    
+    if args.verbose:
+        print_tty_configuration(args)
+    
+    ser = serial.Serial(args.tty_port, args.tty_speed, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
+    
+    
+    try:
+        print_sys_configuration()
+        while True:
+            pkt = ser.read_until(eol_pkt)
+            print_pkt_metadata(pkt)
 
-if args.verbose:
-    print('port set to:{}{}\nbaudrate set to: {}'.format(' '*5, args.tty_port, args.tty_speed))
-
-ser = serial.Serial(args.tty_port, args.tty_speed, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
-
-
-try:
-    while True:
-        pkt = ser.read_until(eol_pkt)
-        print(pkt)
-        print('header: ', pkt[:header_size])
-        #### '0x' +''.join([hex(x)[2:] for x in a.encode()][::-1])
-        addr = pkt[header_size : header_size+address_buf]
-        addr_p = '0x' +''.join(['0' + hex(x)[2:] for x in addr][::-1]) # TODO
-        print('address: ', addr_p)
-        data_pkt = pkt[header_size+address_buf : - eol_size]
-        print('body: ', data_pkt)
-
-
-        # TODO
-        write_to_disk(int(addr_p, 16), data_pkt)
-        print_disk()
-        
-except Exception as e:
-    e.printStackTrace()
-    print('Exiting...')
-
+            #write_to_disk(int(addr_p, 16), data_pkt)
+            #print_disk()
+            
+    except Exception as e:
+        e.printStackTrace()
+        print('Exiting...')
+    
