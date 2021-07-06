23f0ef16535f6d3fb0aaa41f415d6c14cc70c9f8
Mill <millerkjc@gmail.com>
2021-07-06 17:19:53 +0200

    [broker] read + write

--- broker.py
+++ broker.py
@@ -16,13 +16,24 @@ eol_size = len(eol_pkt)
 #disk_size = 1024 * 10  # 5 MB
 disk_size = 1024 * 1024 * 5 # 5 MB
 disk = [b'\x00'] * disk_size
-superblock_address_5MB = 0x4f7bf4
+superblock_address_5MB = 0x4f6bf4
 
+# user gui utilities
+pkts_buffer = []
+pkts_buffer_size = 5
 
 #
 # Signal handler
 #
 def signal_handler(sig, frame):
+    # print('DATA')
+    # print_data()
+    # print('DINODE')
+    # print_dinode()
+    # print_superblock(128)
+    #partial_print()
+    #print('DINODE')
+    #print_disk(superblock_address_5MB-128, superblock_address_5MB)
     print('You pressed Ctrl+C!')
     clean_shutdown()
     sys.exit(0)
@@ -88,7 +99,7 @@ def get_data_pkt(pkt):
 #
 def get_last_data_address():
     last_data_addr = disk[superblock_address_5MB:superblock_address_5MB+4]
-    data_addr = int('0x' +''.join(['{:02x}'.format(ord(x)) for x in last_inode_addr[::-1]]), 16)
+    data_addr = int('0x' +''.join(['{:02x}'.format(ord(x)) for x in last_data_addr[::-1]]), 16)
     return data_addr
 
 def get_last_dinode_address():
@@ -107,7 +118,7 @@ def print_disk(starts=0, ends=disk_size, p_disk_size=4):
 
 def partial_print(p_disk_size=8):
     print('*** Disk ***')
-    print_disk(0, 64, p_disk_size)
+    Bprint_disk(0, 64, p_disk_size)
     print('.....')
     print('*** Superblock ***')
     print_disk(superblock_address_5MB, superblock_address_5MB + 64, p_disk_size)
@@ -140,8 +151,92 @@ def dump_disk():
     pass
 
 
+
+
+
+
+
+
+
+#
+# User
+#
+
+def make_currying(f, a):
+    def fc(*args):
+        return f(a, *args)
+    return fc
+
+broker_utilities = {
+                    0:('print last x pkt (MAX 5)', None),
+                    1:('print superblock', print_superblock),
+                    2:('print data', print_data),
+                    3:('print last data address', get_last_data_address),
+                    4:('print dinode', print_dinode),
+                    5:('print last dinode address', get_last_dinode_address),
+                    6:('print all file name and id', None),
+                    7:('get file x (use the id)', None)
+                   }
+
+def menu_gui():
+    cr_utilities = lambda u,x : ('{}) ' + u).format(x)
+
+    #utilities = (
+    #             'print last x pkt (MAX 5)',
+    #             'print superblock',
+    #             'print data',
+    #             'print last data address',
+    #             'print dinode',
+    #             'print last dinode address',
+    #             'print all file name and id',
+    #             'get file x (use the id)'
+    #            )
+
+    print('=== Broker functionalities ===')
+    for i in range(len(broker_utilities)):
+        print(make_currying(cr_utilities, broker_utilities[i][0])(i+1))
+    print()
+
+
+def write_to_file(f, addr, data):
+    f.seek(addr, 0)
+    f.write(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def argparse_conf(parser):
+#     direct_args = [
+#                    #(       name, type,                                        help)
+#                     ( 'tty_port',  str,          'tty of the connected STM32 board'),
+#                     ('tty_speed',  int, 'baudrate tty of the connected STM32 board')
+#                   ]
+# 
+#     option_str_arg = [
+#                      #(   option_strings,       action,                        help)
+#                       ('-v', '--verbose', 'store_true', 'increase output verbosity')
+#                      ]
+# 
+#     for el in direct_args:
+#         parser.add_argument(el[0], type=el[1], help=el[1])
+# 
+#     for el in option_str_arg:
+#         parser.add_argument(el[0], el[1], action=el[2], help=el[3])
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    #argparse_conf(parser)
     parser.add_argument('tty_port', help='tty of the connected STM32 board')
     parser.add_argument('tty_speed', type=int, help='baudrate tty of the connected STM32 board')
     parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
@@ -155,11 +250,13 @@ if __name__ == '__main__':
     try:
         ser = serial.Serial(args.tty_port, args.tty_speed, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
     except serial.serialutil.SerialException as se:
+        #menu_gui()
         print('{} tty not found'.format(args.tty_port))
         sys.exit(1)
     
+    print_sys_configuration()
+
     try:
-        print_sys_configuration()
         while True:
             pkt = ser.read_until(eol_pkt)
 
@@ -169,13 +266,35 @@ if __name__ == '__main__':
 
             pkt_data = get_data_pkt(pkt)
             disk_addr = get_addr_pkt_wrt(pkt)
-            #write_to_disk(int(disk_addr, 16), pkt_data)
             #print_disk(16)
             #partial_print()
             #print(pkt[:128])
             # print_dinode()
             # print_superblock(64)
-            print_pkt_metadata(pkt)
+            #print_superblock(128)
+
+
+            if pkt[:header_size] != b'zcfs_rdx_':
+                write_to_disk(int(disk_addr, 16), pkt_data)
+                print_pkt_metadata(pkt)
+            else:
+                print("START WRITING")
+                print_pkt_metadata(pkt)
+                start = get_addr_pkt_wrt(pkt)
+                end = get_data_pkt(pkt)
+                end_2 = '0x' +''.join(['{:02x}'.format(x) for x in end[::-1]])
+
+                #print('start: ', start, ", t:", type(start), ", int: ", int(start,16))
+                #print('end_2: ', end_2, ", t:", type(end_2), ", int: ", int(end_2,16))
+
+                ser.write(b''.join(disk[int(start,16):int(start,16) + int(end_2,16)]))
+                print("STOP WRITING")
+
+            print('DATA')
+            print_data()
+            print('DINODE')
+            print_dinode()
+            print_superblock(128)
             
     except Exception as e:
         e.printStackTrace()
